4b035180044a7656443caaccf80f122b373f9178
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-02 12:34:16 -0600

    Fix images stories

--- screens/data/user.js
+++ screens/data/user.js
@@ -7,18 +7,18 @@ export const USERS = [
 
     {
       user: 'MarleniAguilar',
-      image: 'https://i.imgur.com/dxjd7FB.jpg',
+      image: 'https://m.media-amazon.com/images/I/61bdLNRW9eS._AC_SL1500_.jpg',
     },
     {
       user: 'ChecoPereztroica',
-      image: 'https://i.imgur.com/dxjd7FB.jpg',
+      image: 'https://m.media-amazon.com/images/I/51ylAhrp0VL._SL1000_.jpg',
     },
     {
       user: 'MarleneSuley',
-      image: 'https://i.imgur.com/dxjd7FB.jpg',
+      image: 'https://m.media-amazon.com/images/I/71Xt3ItB-SS._AC_SL1500_.jpg',
     },
     {
       user: 'GuguritoÑon',
-      image: 'https://i.imgur.com/dxjd7FB.jpg',
+      image: 'https://m.media-amazon.com/images/I/71UYEfMjUaL._SL1500_.jpg',
     },
 ]
