04b3cf60cb570adb66a465d5d0130323ab74ca15
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-02 12:59:32 -0600

    Add Post component and post.js data

---
+++ screens/data/post.js
@@ -0,0 +1,39 @@
+import { USERS } from './users';
+
+export const POST = [
+    {
+        imageUrl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[0].user,
+        likes: 7384,
+        caption: 'Train ride to Hogwarts.',
+        profile_picture: USERS[O].image,
+        Comment: [
+            {
+                user: 'MarleniAguilar',
+                comment: 'Wow! this is look amazing, I would like have it! hahaha',
+            },
+            {
+                user: 'ChecoPereztroica',
+                comment: "Once, I wake up, I'll  finally be ready to code this up!",
+            }
+        ]  
+    },
+    {
+        imageUrl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[1].user,
+        likes: 8984,
+        caption: 'Train ride to Hogwarts.',
+        profile_picture: USERS[1].image,
+        Comment: [
+            {
+                user: 'GuguritoÑon',
+                comment: "I'm hahaha",
+            },
+            {
+                user: 'MarleneSuley',
+                comment: "I'm sleeping becausa I worked all night, omg, I'm so tired",
+            }
+        ]  
+    },
+
+]
